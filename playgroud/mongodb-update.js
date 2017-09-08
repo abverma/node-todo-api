840a64c7c968bcf9907f31354260a8bc4ba43e6a
abhishek <abhinow.abhishek@gmail.com>
2017-09-09 02:16:06 +0530

    Modiry mongodb-update.js

--- playgroud/mongodb-update.js
+++ playgroud/mongodb-update.js
@@ -6,10 +6,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to mongodb server...');
 
-  db.collection('Users').findOneAndUpdate({age: 32}, {name: 'Abhishek', age: 31, location:'Bengaluru'}).then((result) =>{
+  db.collection('Users').findOneAndUpdate({
+    name: 'Abhishek'
+  }, {
+    $inc: {
+      age: -1
+    }
+  }).then((result) =>{
     console.log(JSON.stringify(result, undefined, 2));
   }, (err) => {
-    console.log('Unable to get todo documents');
+    console.log('Unable to get todo documents', err);
   })
   //db.close();
 });
