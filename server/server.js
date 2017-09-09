a2e95e22c78a5ffc7caaaf9e36c9f88ebea43b4d
abhishek <abhinow.abhishek@gmail.com>
2017-09-09 15:26:41 +0530

    Add express server, mongoose models, POST api for Todos

--- server/server.js
+++ server/server.js
@@ -1,36 +1,27 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{
-  useMongoClient: true
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo(req.body);
+
+  todo.save()
+    .then((doc) => {
+      console.log('Todo saved successfully');
+      res.send(doc);
+    }, (err) => {
+      console.log('Unable to save todo', err);
+      res.status(400).send(err);
+    })
 });
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minLength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+app.listen(3000, () => {
+  console.log('Server listening on port 3000...');
 });
-
-var newTodo = new Todo({
-  text: 'Masturbate  ',
-  completed: false,
-  completedAt: 1234
-});
-
-newTodo.save()
-  .then((doc) => {
-    console.log('Saved doc', doc);
-  }, (err) => {
-    console.log('Unable to save todo', err);
-  });
